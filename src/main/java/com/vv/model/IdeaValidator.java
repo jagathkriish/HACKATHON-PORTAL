706d29659ba4ef10550f95c40acd451ee526b949
Subrahmanyam Baratam <slc@DIN66008961.corp.capgemini.com>
2017-03-01 17:33:12 +0530

    idea submit screen front fields addition

--- src/main/java/com/vv/model/IdeaValidator.java
+++ src/main/java/com/vv/model/IdeaValidator.java
@@ -22,6 +22,45 @@ public class IdeaValidator {
 	@NotNull
 	private MultipartFile docFile;
 	private MultipartFile videoFile;
+	@NotNull
+	@Size(min=2, max=200)
+	private String servicebu;
+	@NotNull
+	@Size(min=2, max=200)
+	private String projectName;
+	@NotNull
+	@Size(min=2, max=200)
+	private String contactNum;
+	@NotNull
+	@Size(min=2, max=200)
+	private String locationName;
+	@NotNull
+	@Size(min=2, max=200)
+	private String problemArea;
+	@NotNull
+	@Size(min=1, max=200)
+	private String[] vertIndus;
+	@Size(min=2, max=200)
+	private String oVertical;
+	@NotNull
+	@Size(min=1, max=200)
+	private String[] farea;
+	@Size(min=2, max=200)
+	private String ofrea;
+	@NotNull
+	@Size(min=2, max=200)
+	private String solTit;
+	@NotNull
+	@Size(min=2, max=200)
+	private String sDesc;
+	@NotNull
+	@Size(min=2, max=200)
+	private String technol;
+	@NotNull
+	@Size(min=2, max=200)
+	private String bubenift;
+	
+	
 	public String getCapId() {
 		return capId;
 	}
@@ -46,6 +85,73 @@ public class IdeaValidator {
 	public void setIdeaThesis(String ideaThesis) {
 		this.ideaThesis = ideaThesis;
 	}
+	public String getServicebu() {
+		return servicebu;
+	}
+	public void setServicebu(String servicebu) {
+		this.servicebu = servicebu;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getContactNum() {
+		return contactNum;
+	}
+	public void setContactNum(String contactNum) {
+		this.contactNum = contactNum;
+	}
+	public String getLocationName() {
+		return locationName;
+	}
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+	public String getProblemArea() {
+		return problemArea;
+	}
+	public void setProblemArea(String problemArea) {
+		this.problemArea = problemArea;
+	}
+	public String[] getVertIndus() {
+		return vertIndus;
+	}
+	public void setVertIndus(String[] vertIndus) {
+		this.vertIndus = vertIndus;
+		System.out.println("LENGHT --> "+vertIndus.length);
+	}
+	public String[] getFarea() {
+		return farea;
+	}
+	public void setFarea(String[] farea) {
+		this.farea = farea;
+	}
+	public String getSolTit() {
+		return solTit;
+	}
+	public void setSolTit(String solTit) {
+		this.solTit = solTit;
+	}
+	public String getsDesc() {
+		return sDesc;
+	}
+	public void setsDesc(String sDesc) {
+		this.sDesc = sDesc;
+	}
+	public String getTechnol() {
+		return technol;
+	}
+	public void setTechnol(String technol) {
+		this.technol = technol;
+	}
+	public String getBubenift() {
+		return bubenift;
+	}
+	public void setBubenift(String bubenift) {
+		this.bubenift = bubenift;
+	}
 	public MultipartFile getDocFile() {
 		return docFile;
 	}
@@ -57,6 +163,18 @@ public class IdeaValidator {
 	}
 	public void setVideoFile(MultipartFile videoFile) {
 		this.videoFile = videoFile;
+	}
+	public String getoVertical() {
+		return oVertical;
+	}
+	public void setoVertical(String oVertical) {
+		this.oVertical = oVertical;
+	}
+	public String getOfrea() {
+		return ofrea;
+	}
+	public void setOfrea(String ofrea) {
+		this.ofrea = ofrea;
 	}
 	@Override
 	public String toString() {
