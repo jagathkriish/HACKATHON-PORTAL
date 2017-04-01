1a63825ed8558842723c763c885d238a09700bec
jagathvenkat <jagath.kristipati@gmail.com>
2017-04-01 19:45:02 +0530

    cleaned and modified for war deploy

--- src/main/java/com/vv/config/WebSecurityConfig.java
+++ src/main/java/com/vv/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
